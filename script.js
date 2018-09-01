75246068b5d469392ba2bc3d1067d862c30b67e1
Anger Dooshima Lois <danielladoosh@gmail.com>
2018-09-01 13:27:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$('body').css("background-color", "#964f8e");
+
+$('body').css("font-family", "verdana");
+
+$('body').css("font-size", "15px");
+
+$('fieldset').css("background-color", "white");
+
+$('fieldset').css("width", "600px");
+
+$('fieldset').css("margin-top", "200px");
+
+$('fieldset').css("margin-left", "300px");
